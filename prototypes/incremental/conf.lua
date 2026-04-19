ecea510850dc29f3a32faf5a4a8c157682922b3b
Guillaume Piolat <contact@auburnsounds.com>
2026-04-19 22:21:33 +0200

    feat: Love2D project skeleton with window config

--- prototypes/incremental/conf.lua
+++ prototypes/incremental/conf.lua
@@ -1,8 +1,9 @@
+-- conf.lua
 function love.conf(t)
-    t.window.title = "Incremental"
-    t.window.width = 1024
-    t.window.height = 720
-    t.window.resizable = true
-    t.window.vsync = 1
-    t.console = true
+  t.window.width = 800
+  t.window.height = 600
+  t.window.title = "Incremental"
+  t.window.vsync = 1
+  t.window.resizable = false
+  t.version = "11.5"
 end
--- prototypes/incremental/main.lua
+++ prototypes/incremental/main.lua
@@ -1,38 +1,20 @@
-local state = {
-    money = 0,
-    perClick = 1,
-    perSecond = 0,
-}
-
-local font
-
+-- main.lua
 function love.load()
-    font = love.graphics.newFont(22)
-    love.graphics.setFont(font)
-    love.graphics.setBackgroundColor(0.08, 0.08, 0.10)
+  -- Will be populated with state initialization
 end
 
 function love.update(dt)
-    state.money = state.money + state.perSecond * dt
+  -- Will dispatch to subsystems
 end
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print(string.format("Money: %.2f", state.money), 20, 20)
-    love.graphics.print(string.format("Per click: %d", state.perClick), 20, 50)
-    love.graphics.print(string.format("Per second: %.1f", state.perSecond), 20, 80)
-    love.graphics.print("[click] earn   [u] +1/s   [r] reset   [esc] quit", 20, 140)
-    love.graphics.print(string.format("FPS: %d", love.timer.getFPS()), 20, 680)
+  -- Will dispatch to subsystems
 end
 
 function love.mousepressed(x, y, button)
-    if button == 1 then
-        state.money = state.money + state.perClick
-    end
+  -- Will route to input system
 end
 
 function love.keypressed(key)
-    if key == "escape" then love.event.quit() end
-    if key == "r" then state.money = 0; state.perSecond = 0 end
-    if key == "u" then state.perSecond = state.perSecond + 1 end
+  -- Will route to input system
 end
