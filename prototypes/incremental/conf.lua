1ad60b956520b87a43b11515d02c418de7f1e287
Guillaume Piolat <contact@auburnsounds.com>
2026-04-19 23:39:43 +0200

    feat: resizable window; camera uses dynamic dimensions

--- prototypes/incremental/conf.lua
+++ prototypes/incremental/conf.lua
@@ -4,6 +4,6 @@ function love.conf(t)
   t.window.height = 600
   t.window.title = "Incremental"
   t.window.vsync = 1
-  t.window.resizable = false
+  t.window.resizable = true
   t.version = "11.5"
 end
--- prototypes/incremental/src/camera.lua
+++ prototypes/incremental/src/camera.lua
@@ -6,9 +6,10 @@ local EDGE_SCROLL_SPEED = 150
 
 function camera.update(dt, state)
   local mx, my = love.mouse.getPosition()
+  local sw, sh = love.graphics.getDimensions()
   if mx < EDGE_MARGIN then
     state.camera.x = state.camera.x - EDGE_SCROLL_SPEED * dt
-  elseif mx > 800 - EDGE_MARGIN then
+  elseif mx > sw - EDGE_MARGIN then
     state.camera.x = state.camera.x + EDGE_SCROLL_SPEED * dt
   end
   if love.keyboard.isDown("left") then
@@ -29,8 +30,9 @@ end
 
 function camera.attach(state)
   local zoom = state.camera.zoom
+  local sw, sh = love.graphics.getDimensions()
   love.graphics.push()
-  love.graphics.translate(400, 300)  -- Center of 800x600 window
+  love.graphics.translate(sw / 2, sh / 2)
   love.graphics.scale(zoom, zoom)
   love.graphics.translate(-state.camera.x, -state.camera.y)
 end
@@ -41,8 +43,9 @@ end
 
 function camera.screen_to_world(state, screen_x, screen_y)
   local zoom = state.camera.zoom
-  local world_x = (screen_x - 400) / zoom + state.camera.x
-  local world_y = (screen_y - 300) / zoom + state.camera.y
+  local sw, sh = love.graphics.getDimensions()
+  local world_x = (screen_x - sw / 2) / zoom + state.camera.x
+  local world_y = (screen_y - sh / 2) / zoom + state.camera.y
   return world_x, world_y
 end
 
